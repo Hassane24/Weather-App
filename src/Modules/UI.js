bd1312416c12c9fc99e31ff64e152f00e6b0e9ce
Hassane24 <hsisane5@gmail.com>
2022-04-09 01:07:34 +0200

    removed if statements

--- src/Modules/UI.js
+++ src/Modules/UI.js
@@ -17,8 +17,6 @@ const UI = (() => {
       neededData(
         `https://api.openweathermap.org/data/2.5/weather?q=${e.target.innerText}&APPID=512dd7c72356c3b72f04ac7af77e9f8a&units=metric`
       ).then((res) => {
-        if (res.cod === "404") return console.log(res.message);
-        if (res.cod === "400") return console.log(res.message);
         console.log(res);
       });
     });
