73ebae6a5ab94de67abb9a0914a5c05718a814ab
Hassane24 <hsisane5@gmail.com>
2022-04-08 19:41:34 +0200

    UI module

---
+++ src/Modules/UI.js
@@ -0,0 +1,14 @@
+const UI = (() => {
+  const searchBar = document.querySelector("#searchBar");
+  const searchButton = document.querySelector("#startSearch");
+  const temp = document.querySelector("#temp");
+  const location = document.querySelector("#location");
+  const date = document.querySelector("#date");
+  const weatherIcon = document.querySelector("#weather-icon");
+  const weatherState = document.querySelector("#weather-state");
+  const states = document.querySelectorAll("[data-state]");
+
+  return {};
+})();
+
+export { UI };
