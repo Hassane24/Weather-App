1e45174d8c9f3a7b40bb5e1b6b290546401680e4
Hassane24 <hsisane5@gmail.com>
2022-04-09 02:57:52 +0200

    display function almost done

--- src/Modules/UI.js
+++ src/Modules/UI.js
@@ -9,6 +9,9 @@ const UI = (() => {
   const weatherIcon = document.querySelector("#weather-icon");
   const weatherState = document.querySelector("#weather-state");
   const states = document.querySelectorAll("[data-state]");
+  const wind = document.querySelector("#wind");
+  const humidity = document.querySelector("#humidity");
+  const clouds = document.querySelector("#clouds");
 
   function loadHomePage() {}
 
@@ -17,6 +20,7 @@ const UI = (() => {
       neededData(
         `https://api.openweathermap.org/data/2.5/weather?q=${e.target.innerText}&APPID=512dd7c72356c3b72f04ac7af77e9f8a&units=metric`
       ).then((res) => {
+        displayData(res);
         console.log(res);
       });
     });
@@ -30,12 +34,22 @@ const UI = (() => {
         if (res.cod === "400") return alert(res.message);
         if (res.cod === "404") return alert(res.message);
         console.log(res);
+        displayData(res);
       },
       (err) => {
         console.log(err);
       }
     );
   });
+
+  function displayData(data) {
+    location.textContent = data.name;
+    temp.textContent = `${data.main.temp}°C`;
+    weatherState.textContent = data.weather[0].main;
+    clouds.textContent = `${data.clouds.all}%`;
+    humidity.textContent = `${data.main.humidity}%`;
+    wind.textContent = `${Math.round(data.wind.speed * 3.6 * 100) / 100}km/h`;
+  }
 
   return { loadHomePage };
 })();
